1bd90df483ceccbd2327d09a62e744f558988d15
Jonathan Campos <jonathan.campos@concrete.com.br>
2018-03-22 17:05:00 -0300

    Change ViewModel to create repository just once

--- app/src/main/java/com/jhowcs/githubapp/viewmodel/MainViewModel.java
+++ app/src/main/java/com/jhowcs/githubapp/viewmodel/MainViewModel.java
@@ -2,6 +2,7 @@ package com.jhowcs.githubapp.viewmodel;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
+import android.util.Log;
 
 import com.jhowcs.githubapp.model.GithubModel;
 import com.jhowcs.githubapp.repository.network.GithubRepository;
@@ -10,11 +11,20 @@ public class MainViewModel extends ViewModel {
 
     private GithubRepository repository;
 
+    private LiveData<GithubModel> repositoryLiveData;
+
+    private static final String TAG = "MainViewModel";
+
     public MainViewModel(GithubRepository repository) {
         this.repository = repository;
     }
 
-    public LiveData<GithubModel> getAndroidRepostitories() {
-        return repository.getAllAndroidRepositories();
+    public LiveData<GithubModel> getAndroidRepositories() {
+        if (repositoryLiveData == null) {
+            repositoryLiveData = repository.getAllAndroidRepositories();
+            Log.d(TAG, "init repositoryLiveData");
+        }
+        Log.d(TAG, "return repositoryLiveData created previously");
+        return repositoryLiveData;
     }
 }
