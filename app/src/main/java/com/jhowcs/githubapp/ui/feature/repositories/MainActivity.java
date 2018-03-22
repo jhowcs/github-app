a067b7255ace6181b1d4ec87cc002aaa58701abf
jonathan <jhowcs@hotmail.com>
2018-03-22 00:25:57 -0300

    Create view model usin factory and observe changes from repository

--- app/src/main/java/com/jhowcs/githubapp/ui/feature/repositories/MainActivity.java
+++ app/src/main/java/com/jhowcs/githubapp/ui/feature/repositories/MainActivity.java
@@ -1,17 +1,30 @@
 package com.jhowcs.githubapp.ui.feature.repositories;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.jhowcs.githubapp.R;
+import com.jhowcs.githubapp.model.GithubModel;
+import com.jhowcs.githubapp.model.Item;
 import com.jhowcs.githubapp.repository.network.GithubRepository;
+import com.jhowcs.viewmodel.MainViewModel;
+import com.jhowcs.viewmodel.MainViewModelFactory;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private RecyclerView recyclerView;
     private GithubAdapter adapter;
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +34,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.rvRepositories);
         initList();
 
-        GithubRepository repository = new GithubRepository();
-        repository.getAllAndroidRepositories();
+        MainViewModelFactory factory = new MainViewModelFactory(new GithubRepository());
+        viewModel = ViewModelProviders.of(this, factory).get(MainViewModel.class);
+        viewModel.getAndroidRepostitories().observe(this, new Observer<GithubModel>() {
+            @Override
+            public void onChanged(@Nullable GithubModel githubModel) {
+                List<Item> items = githubModel.getItems();
+
+                for (Item item : items)
+                    Log.d(TAG, "onChanged: " + item.getFullName());
+            }
+        });
     }
 
     private void initList() {
